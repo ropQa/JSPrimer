5a3438d7a2824fd5c8d2e6fb031b9e02c6a83e4f
renkataoka <530lotus@gmail.com>
2021-01-16 15:47:45 +0900

    Add 'Display user info' function

--- Github-searcher/index.js
+++ Github-searcher/index.js
@@ -16,9 +16,52 @@ function fetchUserInfo(userId) {
                 // Http OK
                 return response.json().then(userInfo => {
                     console.log(userInfo);
+                    // Create Html strings
+                    const view = escapeHTML`
+                    <h4>${userInfo.name} (@${userInfo.login})</h4>
+                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+                    <dl>
+                        <dt>Location</dt>
+                        <dd>${userInfo.location}</dd>
+                        <dt>Repositories</dt>
+                        <dd>${userInfo.public_repos}</dd>
+                    </dl>
+                    `
+                    // Insert view to the "result" element.
+                    const result = document.getElementById("result");
+                    result.innerHTML = view;
                 });
             }
         }).catch(error => {
             console.error(error);
         });
+}
+
+/**
+ * 
+ * @param {*} str : To be escaped string
+ */
+function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+/**
+ * 
+ * @param {*} strings : Template strings
+ * @param  {...any} values : Each value
+ */
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, str, i) => {
+        const value = values[i - 1];
+        if (typeof value === "string") {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    });
 }
