2eb3a776d991d402b9378dc11c382ab966b2c5a3
renkataoka <530lotus@gmail.com>
2021-01-16 15:02:09 +0900

    Add fetchUserInfo() method

--- Github-searcher/index.js
+++ Github-searcher/index.js
@@ -1,1 +1,24 @@
 console.log("index.js: loaded");
+
+/**
+ * 
+ * @param {*} userId : Github user ID
+ */
+function fetchUserInfo(userId) {
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            console.log(response.status);
+
+            // Http Error handling
+            if (!response.ok) {
+                console.error("Error response", response);
+            } else {
+                // Http OK
+                return response.json().then(userInfo => {
+                    console.log(userInfo);
+                });
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+}
