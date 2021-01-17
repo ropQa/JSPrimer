3664426b456face1387442e6bd6f2478b71003f4
renkataoka <530lotus@gmail.com>
2021-01-17 12:09:19 +0900

    Apply Promise chain.

--- Github-searcher/index.js
+++ Github-searcher/index.js
@@ -1,40 +1,55 @@
 console.log("index.js: loaded");
 
+async function main() {
+    try {
+        const userId = getUserId();
+        const userInfo = await fetchUserInfo(userId);
+        const view = createView(userInfo);
+        displayView(view);
+    } catch(error) {
+        console.error(`Error occures! (${error})`);
+    }
+}
+
+function getUserId() {
+    return document.getElementById("userId").value;
+}
+
 /**
  * 
  * @param {*} userId : Github user ID
  */
 function fetchUserInfo(userId) {
-    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
         .then(response => {
             console.log(response.status);
 
             // Http Error handling
             if (!response.ok) {
-                console.error("Error response", response);
+                return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
             } else {
                 // Http OK
-                return response.json().then(userInfo => {
-                    console.log(userInfo);
-                    // Create Html strings
-                    const view = escapeHTML`
-                    <h4>${userInfo.name} (@${userInfo.login})</h4>
-                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-                    <dl>
-                        <dt>Location</dt>
-                        <dd>${userInfo.location}</dd>
-                        <dt>Repositories</dt>
-                        <dd>${userInfo.public_repos}</dd>
-                    </dl>
-                    `
-                    // Insert view to the "result" element.
-                    const result = document.getElementById("result");
-                    result.innerHTML = view;
-                });
+                return response.json();
             }
-        }).catch(error => {
-            console.error(error);
         });
+}
+
+function createView(userInfo) {
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view) {
+    const result = document.getElementById("result");
+    result.innerHTML = view;
 }
 
 /**
